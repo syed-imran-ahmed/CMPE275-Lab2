ea447c9fe416115665a1d2a44bb7c85730665086
syed-imran-ahmed <imran.vatic@gmail.com>
2017-05-06 23:35:43 -0700

    Some more unit tests for ReservationController.

--- src/test/java/edu/sjsu/cmpe275/lab2/controller/ReservationControllerUnitTests.java
+++ src/test/java/edu/sjsu/cmpe275/lab2/controller/ReservationControllerUnitTests.java
@@ -1,5 +1,6 @@
 package edu.sjsu.cmpe275.lab2.controller;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,6 +19,8 @@ import edu.sjsu.cmpe275.lab2.model.Flight;
 import edu.sjsu.cmpe275.lab2.model.Passenger;
 import edu.sjsu.cmpe275.lab2.model.Plane;
 import edu.sjsu.cmpe275.lab2.model.Reservation;
+import edu.sjsu.cmpe275.lab2.service.FlightService;
+import edu.sjsu.cmpe275.lab2.service.PassengerService;
 import edu.sjsu.cmpe275.lab2.service.ReservationService;
 
 
@@ -36,7 +39,9 @@ import edu.sjsu.cmpe275.lab2.service.ReservationService;
 public class ReservationControllerUnitTests {
 
 	ReservationController controller;
-	ReservationService service;
+	FlightService flightService;
+	ReservationService reservationService;
+	PassengerService passengerService;
 	
 	
 	private static final Long PASSENGER_ID = 1L;
@@ -59,19 +64,25 @@ public class ReservationControllerUnitTests {
 	private static String arrivalTime = "2017-09-03-22";
 	private static String description = "My flight";
 	
+	private static List<String> flightNumbers = Arrays.asList(flightNumber);
+	
 	private static Long orderNumber = 123L;
 	
 	@Before
 	public void setUp(){
-		service = PowerMockito.mock(ReservationService.class);
+		reservationService = PowerMockito.mock(ReservationService.class);
+		passengerService = PowerMockito.mock(PassengerService.class);
+		flightService = PowerMockito.mock(FlightService.class);
 		controller = new ReservationController();
-		controller.reservationService = service;
+		controller.reservationService = reservationService;
+		controller.passengerService = passengerService;
+		controller.flightService = flightService;
 	}
 	
 	//GET
 	@Test
 	public void test_get_reservation_nonexisting() throws JsonProcessingException{
-		Mockito.when(service.getById(orderNumber)).thenReturn(null);
+		Mockito.when(reservationService.getById(orderNumber)).thenReturn(null);
 		ResponseEntity<?> responseEntity = controller.getReservation(orderNumber);
 		HttpStatus status = responseEntity.getStatusCode();
 		Assert.assertEquals(HttpStatus.NOT_FOUND, status);
@@ -102,7 +113,7 @@ public class ReservationControllerUnitTests {
 		passenger.setReservations(Collections.singletonList(reservation));
 		flight.setReservations(Collections.singletonList(reservation));
 		
-		Mockito.when(service.getById(orderNumber)).thenReturn(reservation);
+		Mockito.when(reservationService.getById(orderNumber)).thenReturn(reservation);
 		
 		ResponseEntity<?> searchResponseEntity = controller.getReservation(orderNumber);
 		
@@ -147,7 +158,7 @@ public class ReservationControllerUnitTests {
 		passenger.setReservations(Collections.singletonList(reservation));
 		flight.setReservations(Collections.singletonList(reservation));
 		
-		Mockito.when(service.searchReservations(PASSENGER_ID, from, to, flightNumber)).thenReturn(Collections.singletonList(reservation));
+		Mockito.when(reservationService.searchReservations(PASSENGER_ID, from, to, flightNumber)).thenReturn(Collections.singletonList(reservation));
 		
 		ResponseEntity<?> searchResponseEntity = controller.searchReservation(PASSENGER_ID, from, to, flightNumber);
 		
@@ -166,59 +177,241 @@ public class ReservationControllerUnitTests {
 	
 	//DELETE
 	@Test
-	public void test_delete_reservation_nonexisting(){
+	public void test_delete_reservation_nonexisting() throws JsonProcessingException{
+		Mockito.when(reservationService.getById(orderNumber)).thenReturn(null);
 		
+		ResponseEntity<?> deleteResponseEntity = controller.deleteReservation(orderNumber);
+		
+		HttpStatus status = deleteResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.NOT_FOUND, status);
 	}
 	
 	@Test
-	public void test_delete_reservation_existing(){
+	public void test_delete_reservation_existing() throws JsonProcessingException{
+        Passenger passenger = new Passenger(FIRSTNAME, LASTNAME, AGE, GENDER, PHONE);
+		
+		Plane plane = new Plane(capacity, model, manufacturer, yearOfManufacture);
+		
+		Flight flight = new Flight();
+		flight.setNumber(flightNumber);
+		flight.setPrice(price);
+		flight.setFromOrigin(from);
+		flight.setToDestination(to);
+		flight.setDepartureTime(departureTime);
+		flight.setArrivalTime(arrivalTime);
+		flight.setDescription(description);
+		flight.setSeatsLeft(capacity-1);
+		flight.setPlane(plane);
+		
+		Reservation reservation = new Reservation();
+		reservation.setPassenger(passenger);
+		reservation.setPrice(flight.getPrice());
+		reservation.setFlights(Collections.singletonList(flight));
+		
+		passenger.setReservations(Collections.singletonList(reservation));
+		flight.setReservations(Collections.singletonList(reservation));
+		
+		Mockito.when(reservationService.getById(orderNumber)).thenReturn(reservation);
+		
+		ResponseEntity<?> deleteResponseEntity = controller.deleteReservation(orderNumber);
+		
+		HttpStatus status = deleteResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.OK, status);
 		
 	}
 	
 	//POST
 	@Test
-	public void test_make_reservation_nonexisting_passenger(){
+	public void test_make_reservation_nonexisting_passenger() throws JsonProcessingException{
+		Mockito.when(passengerService.getById(PASSENGER_ID)).thenReturn(null);
 		
+		ResponseEntity<?> makeResponseEntity = controller.makeReservation(PASSENGER_ID, flightNumbers);
+		
+		HttpStatus status = makeResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.BAD_REQUEST, status);
 	}
 	
 	@Test
-	public void test_make_reservation_nonexisting_flights(){
+	public void test_make_reservation_nonexisting_flights() throws JsonProcessingException{
+		Passenger passenger = new Passenger(FIRSTNAME, LASTNAME, AGE, GENDER, PHONE);
+		passenger.setReservations(Collections.emptyList());
 		
+		Mockito.when(passengerService.getById(PASSENGER_ID)).thenReturn(passenger);
+		
+		Mockito.when(flightService.getById(flightNumber)).thenReturn(null);
+		
+		ResponseEntity<?> makeResponseEntity = controller.makeReservation(PASSENGER_ID, flightNumbers);
+		
+		HttpStatus status = makeResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.BAD_REQUEST, status);
 	}
 	
 	@Test
-	public void test_make_reservation_flight_capacity_exceeded(){
+	public void test_make_reservation_flight_capacity_exceeded() throws JsonProcessingException{
+		Passenger passenger = new Passenger(FIRSTNAME, LASTNAME, AGE, GENDER, PHONE);
+		passenger.setReservations(Collections.emptyList());
 		
+		Plane plane = new Plane(capacity, model, manufacturer, yearOfManufacture);
+		
+		Flight flight = new Flight();
+		flight.setNumber(flightNumber);
+		flight.setPrice(price);
+		flight.setFromOrigin(from);
+		flight.setToDestination(to);
+		flight.setDepartureTime(departureTime);
+		flight.setArrivalTime(arrivalTime);
+		flight.setDescription(description);
+		flight.setSeatsLeft(0); //No seats left in this flight
+		flight.setPlane(plane);
+		
+		Mockito.when(passengerService.getById(PASSENGER_ID)).thenReturn(passenger);
+		
+		Mockito.when(flightService.getById(flightNumber)).thenReturn(flight);
+		
+		ResponseEntity<?> makeResponseEntity = controller.makeReservation(PASSENGER_ID, flightNumbers);
+		
+		HttpStatus status = makeResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.BAD_REQUEST, status);	
 	}
 	
 	@Test
-	public void test_make_reservation_conflicting_flights_inter(){
+	public void test_make_reservation_conflicting_flights_inter() throws JsonProcessingException{
+		Passenger passenger = new Passenger("first", "person", AGE, GENDER, PHONE);
+		passenger.setReservations(Collections.emptyList());
+
+		Plane plane = new Plane(capacity, model, manufacturer, yearOfManufacture);
 		
+		String anotherFlightNumber = "XYZ123";
+		
+		Flight flight1 = new Flight();
+		flight1.setNumber(flightNumber);
+		flight1.setPrice(price);
+		flight1.setFromOrigin(from);
+		flight1.setToDestination(to);
+		flight1.setDepartureTime("2017-01-01-00");
+		flight1.setArrivalTime("2017-01-01-02");
+		flight1.setDescription(description);
+		flight1.setSeatsLeft(capacity);
+		flight1.setPlane(plane);
+		
+		Flight flight2 = new Flight();
+		flight2.setNumber(anotherFlightNumber);
+		flight2.setPrice(price);
+		flight2.setFromOrigin(from);
+		flight2.setToDestination(to);
+		flight2.setDepartureTime("2017-01-01-01");
+		flight2.setArrivalTime("2017-01-01-04");
+		flight2.setDescription(description);
+		flight2.setSeatsLeft(capacity);
+		flight2.setPlane(plane);
+		
+		Mockito.when(passengerService.getById(PASSENGER_ID)).thenReturn(passenger);
+		Mockito.when(flightService.getById(flightNumber)).thenReturn(flight1);
+		Mockito.when(flightService.getById(anotherFlightNumber)).thenReturn(flight2);
+		Mockito.when(flightService.checkIfOverlappingFlightTimes(Mockito.anyList(), Mockito.anyList())).thenReturn(true);
+
+		List<String> flightNumbers = Arrays.asList(flightNumber, anotherFlightNumber);
+		ResponseEntity<?> makeResponseEntity = controller.makeReservation(PASSENGER_ID, flightNumbers);
+		
+		HttpStatus status = makeResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.BAD_REQUEST, status);
 	}
 	
 	@Test
-	public void test_make_reservation_conflicting_flights_intra(){
+	public void test_make_reservation_conflicting_flights_intra() throws JsonProcessingException{
+		Passenger passenger = new Passenger("first", "person", AGE, GENDER, PHONE);
+		passenger.setReservations(Collections.emptyList());
+
+		Plane plane = new Plane(capacity, model, manufacturer, yearOfManufacture);
 		
+		String anotherFlightNumber = "XYZ123";
+		
+		Flight flight1 = new Flight();
+		flight1.setNumber(flightNumber);
+		flight1.setPrice(price);
+		flight1.setFromOrigin(from);
+		flight1.setToDestination(to);
+		flight1.setDepartureTime("2017-01-01-00");
+		flight1.setArrivalTime("2017-01-01-02");
+		flight1.setDescription(description);
+		flight1.setSeatsLeft(capacity);
+		flight1.setPlane(plane);
+		
+		Reservation reservation = new Reservation();
+		reservation.setPassenger(passenger);
+		reservation.setPrice(flight1.getPrice());
+		reservation.setFlights(Collections.singletonList(flight1));
+		
+		passenger.setReservations(Collections.singletonList(reservation));
+		flight1.setReservations(Collections.singletonList(reservation));
+		
+		Flight flight2 = new Flight();
+		flight2.setNumber(anotherFlightNumber);
+		flight2.setPrice(price);
+		flight2.setFromOrigin(from);
+		flight2.setToDestination(to);
+		flight2.setDepartureTime("2017-01-01-01");
+		flight2.setArrivalTime("2017-01-01-04");
+		flight2.setDescription(description);
+		flight2.setSeatsLeft(capacity);
+		flight2.setPlane(plane);
+		
+		Mockito.when(passengerService.getById(PASSENGER_ID)).thenReturn(passenger);
+		Mockito.when(flightService.getById(flightNumber)).thenReturn(flight1);
+		Mockito.when(flightService.getById(anotherFlightNumber)).thenReturn(flight2);
+		Mockito.when(flightService.checkIfOverlappingFlightTimes(Mockito.anyList(), Mockito.anyList())).thenReturn(true);
+
+		List<String> flightNumbers = Arrays.asList(anotherFlightNumber);
+		ResponseEntity<?> makeResponseEntity = controller.makeReservation(PASSENGER_ID, flightNumbers);
+		
+		HttpStatus status = makeResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.BAD_REQUEST, status);
 	}
 	
 	//UPDATE
 	@Test
-	public void test_update_reservation_nonexisting(){
+	public void test_update_reservation_nonexisting() throws JsonProcessingException{
+		List<String> flightsAdded = Arrays.asList("XYZ123");
+		List<String> flightsRemoved = Arrays.asList("ABC123");
 		
+		Mockito.when(reservationService.getById(orderNumber)).thenReturn(null);
+		
+		ResponseEntity<?> updateResponseEntity = controller.updateReservation(orderNumber, flightsAdded, flightsRemoved);
+		
+		HttpStatus status = updateResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.NOT_FOUND, status);
 	}
 	
 	@Test
-	public void test_update_reservation_empty_flights_list(){
+	public void test_update_reservation_empty_flights_list() throws JsonProcessingException{
+		Passenger passenger = new Passenger("first", "person", AGE, GENDER, PHONE);
+
+		Plane plane = new Plane(capacity, model, manufacturer, yearOfManufacture);
 		
-	}
-	
-	@Test
-	public void test_update_reservation_with_conflicting_flights(){
+		Flight flight = new Flight();
+		flight.setNumber(flightNumber);
+		flight.setPrice(price);
+		flight.setFromOrigin(from);
+		flight.setToDestination(to);
+		flight.setDepartureTime("2017-01-01-00");
+		flight.setArrivalTime("2017-01-01-02");
+		flight.setDescription(description);
+		flight.setSeatsLeft(capacity);
+		flight.setPlane(plane);
 		
-	}
-	
-	@Test
-	public void test_update_reservations_no_conflict(){
+		Reservation reservation = new Reservation();
+		reservation.setPassenger(passenger);
+		reservation.setPrice(flight.getPrice());
+		reservation.setFlights(Collections.singletonList(flight));
 		
+		List<String> flightsAdded = Collections.emptyList();
+		List<String> flightsRemoved = Arrays.asList(flightNumber);
+		
+		Mockito.when(reservationService.getById(orderNumber)).thenReturn(reservation);
+		
+		ResponseEntity<?> updateResponseEntity = controller.updateReservation(orderNumber, flightsAdded, flightsRemoved);
+		
+		HttpStatus status = updateResponseEntity.getStatusCode();
+		Assert.assertEquals(HttpStatus.BAD_REQUEST, status);
 	}
 }
